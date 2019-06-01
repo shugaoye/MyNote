e85f79b8ec32416dac68ebc2f112dfaf9279728c
Roger Ye <shugaoye@yahoo.com>
2019-06-01 23:15:56 +0800

    added Python env.

---
+++ env.bat
@@ -0,0 +1,4 @@
+REM install tushare using command
+REM pip install --user tushare -i https://pypi.tuna.tsinghua.edu.cn/simple
+
+set PATH=C:\Users\%USERNAME%\AppData\Roaming\Python\Python37\Scripts;"C:\Program Files (x86)\Python37-32";"C:\Program Files (x86)\Python37-32\Scripts";%PATH%
